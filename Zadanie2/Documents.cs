9bfd96420af8e5f2235258640b2b071fd9b7e080
Marina <venskaanika@gmail.com>
2025-06-15 23:25:38 +0300

    2 Zadania

---
+++ Zadanie2/Documents.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    public interface IDocument
+    {
+        enum FormatType { TXT, PDF, JPG }
+
+        string GetFileName();
+        void SetFileName(string fileName);
+    }
+
+    public class PDFDocument : IDocument
+    {
+        private string fileName;
+
+        public PDFDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public string GetFileName() => fileName;
+        public void SetFileName(string fileName) => this.fileName = fileName;
+    }
+
+    public class TextDocument : IDocument
+    {
+        private string fileName;
+
+        public TextDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public string GetFileName() => fileName;
+        public void SetFileName(string fileName) => this.fileName = fileName;
+    }
+
+    public class ImageDocument : IDocument
+    {
+        private string fileName;
+
+        public ImageDocument(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public string GetFileName() => fileName;
+        public void SetFileName(string fileName) => this.fileName = fileName;
+    }
+}
---
+++ Zadanie2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var multiDevice = new MultifunctionalDevice();
+            multiDevice.PowerOn();
+
+            IDocument doc1 = new PDFDocument("doc1.pdf");
+            multiDevice.Print(doc1);
+
+            IDocument doc2;
+            multiDevice.Scan(out doc2, IDocument.FormatType.PDF);
+
+            multiDevice.ScanAndPrint();
+
+            IDocument doc3 = new TextDocument("fax1.txt");
+            multiDevice.SendFax(doc3);
+
+            Console.WriteLine(multiDevice.Counter);
+            Console.WriteLine(multiDevice.PrintCounter);
+            Console.WriteLine(multiDevice.ScanCounter);
+            Console.WriteLine(multiDevice.FaxCounter);
+        }
+    }
+}
