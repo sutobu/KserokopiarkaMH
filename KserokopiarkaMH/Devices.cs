9954ec361839d40e0a4c23634b926177be96325f
Marina <venskaanika@gmail.com>
2025-06-15 23:09:23 +0300

    Add project files.

---
+++ KserokopiarkaMH/Devices.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    public interface IDevice
+    {
+        enum State { on, off }
+
+        void PowerOn();
+        void PowerOff();
+        State GetState();
+    }
+
+    public interface IPrinter : IDevice
+    {
+        void Print(in IDocument doc);
+        int PrintCounter { get; }
+    }
+
+    public interface IScanner : IDevice
+    {
+        void Scan(out IDocument doc, IDocument.FormatType format = IDocument.FormatType.JPG);
+        int ScanCounter { get; }
+    }
+
+    public class Copier : IPrinter, IScanner
+    {
+        private IDevice.State state = IDevice.State.off;
+        private int printCounter = 0;
+        private int scanCounter = 0;
+        private int counter = 0;
+
+        public int Counter => counter;
+        public int PrintCounter => printCounter;
+        public int ScanCounter => scanCounter;
+
+        public IDevice.State GetState() => state;
+
+        public void PowerOn()
+        {
+            if (state == IDevice.State.off)
+            {
+                state = IDevice.State.on;
+                counter++;
+            }
+        }
+
+        public void PowerOff()
+        {
+            if (state == IDevice.State.on)
+            {
+                state = IDevice.State.off;
+            }
+        }
+
+        public void Print(in IDocument doc)
+        {
+            if (state != IDevice.State.on) return;
+
+            printCounter++;
+            Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Print: {doc.GetFileName()}");
+        }
+
+        public void Scan(out IDocument doc, IDocument.FormatType format = IDocument.FormatType.JPG)
+        {
+            doc = null;
+            if (state != IDevice.State.on) return;
+
+            scanCounter++;
+            string fileName;
+            switch (format)
+            {
+                case IDocument.FormatType.PDF:
+                    fileName = $"PDFScan{scanCounter:D4}.pdf";
+                    doc = new PDFDocument(fileName);
+                    break;
+                case IDocument.FormatType.TXT:
+                    fileName = $"TextScan{scanCounter:D4}.txt";
+                    doc = new TextDocument(fileName);
+                    break;
+                case IDocument.FormatType.JPG:
+                default:
+                    fileName = $"ImageScan{scanCounter:D4}.jpg";
+                    doc = new ImageDocument(fileName);
+                    break;
+            }
+            Console.WriteLine($"{DateTime.Now:dd.MM.yyyy HH:mm:ss} Scan: {fileName}");
+        }
+
+        public void ScanAndPrint()
+        {
+            if (state != IDevice.State.on) return;
+
+            IDocument doc;
+            Scan(out doc, IDocument.FormatType.JPG);
+            if (doc != null)
+            {
+                Print(doc);
+            }
+        }
+    }
+}
---
+++ KserokopiarkaMH/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var xerox = new Copier();
+            xerox.PowerOn();
+            IDocument doc1 = new PDFDocument("aaa.pdf");
+            xerox.Print(doc1);
+
+            IDocument doc2;
+            xerox.Scan(out doc2);
+
+            xerox.ScanAndPrint();
+            Console.WriteLine(xerox.Counter);
+            Console.WriteLine(xerox.PrintCounter);
+            Console.WriteLine(xerox.ScanCounter);
+        }
+    }
+}
